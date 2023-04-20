b69a139271160f64e2b82e3f82a8f222c538c86f
kukicro1 <krunomraz@gmail.com>
2023-04-20 16:44:12 +0200

    Updated FindingsInfo, deleted getting el by id

--- src/components/FindingsInfo.tsx
+++ src/components/FindingsInfo.tsx
@@ -25,138 +25,30 @@ export default function FindingsInfo({
   foundHard,
   foundVeryHard,
 }: ExtendedGameProps) {
+  const difficultyLevels = [
+    { difficulty: 'easy', found: foundEasy },
+    { difficulty: 'medium', found: foundMedium },
+    { difficulty: 'hard', found: foundHard },
+    { difficulty: 'veryHard', found: foundVeryHard },
+  ]
+
   return (
     <div className={FindingsInfoCSS.findings}>
       <ul className={FindingsInfoCSS.ul}>
-        <li
-          id={game?.names.easy}
-          className={FindingsInfoCSS.li + ' ' + foundEasy}
-        >
-          <img
-            className={FindingsInfoCSS.img}
-            src={game?.images.easy}
-            alt={game?.names.easy}
-          />
-          {game?.names.easy}
-        </li>
-        <li
-          id={game?.names.medium}
-          className={FindingsInfoCSS.li + ' ' + foundMedium}
-        >
-          <img
-            className={FindingsInfoCSS.img}
-            src={game?.images.medium}
-            alt={game?.names.medium}
-          />
-          {game?.names.medium}
-        </li>
-        <li
-          id={game?.names.hard}
-          className={FindingsInfoCSS.li + ' ' + foundHard}
-        >
-          <img
-            className={FindingsInfoCSS.img}
-            src={game?.images.hard}
-            alt={game?.names.hard}
-          />
-          {game?.names.hard}
-        </li>
-        <li
-          id={game?.names.veryHard}
-          className={FindingsInfoCSS.li + ' ' + foundVeryHard}
-        >
-          <img
-            className={FindingsInfoCSS.img}
-            src={game?.images.veryHard}
-            alt={game?.names.veryHard}
-          />
-          {game?.names.veryHard}
-        </li>
+        {difficultyLevels.map((level) => (
+          <li
+            key={level.difficulty}
+            className={FindingsInfoCSS.li + ' ' + level.found}
+          >
+            <img
+              className={FindingsInfoCSS.img}
+              src={game?.images[level.difficulty]}
+              alt={game?.names[level.difficulty]}
+            />
+            {game?.names[level.difficulty]}
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
-
-// const [undiscoveredCharacters, setUndiscoveredCharacters] = useState<
-//   (string | undefined)[]
-// >([
-//   game?.names.easy,
-//   game?.names.medium,
-//   game?.names.hard,
-//   game?.names.veryHard,
-// ])
-// const [undiscoveredCharacters, setUndiscoveredCharacters] = useState<
-//   (string | undefined)[]
-// >([
-//   game?.images.easy,
-//   game?.images.medium,
-//   game?.images.hard,
-//   game?.images.veryHard,
-// ])
-
-/* {undiscoveredCharacters.map((character) => {
-          return (
-            <li key={character} className={FindingsInfoCSS.li}>
-              <img
-                className={FindingsInfoCSS.img}
-                src={game?.images.character}
-                alt={character}
-              />
-              {character}
-            </li>
-          )
-        })} */
-
-// import { useEffect, useState } from 'react'
-// import FindingsInfoCSS from './FindingsInfo.module.css'
-// import { GameProps } from './Navbar'
-
-// interface ExtendedGameProps extends GameProps {
-//   remainingCharactersStatus: boolean
-// }
-
-// export default function FindingsInfo({
-//   game,
-//   foundCharacters,
-//   remainingCharactersStatus,
-// }: ExtendedGameProps) {
-//   const [foundCharactersState, setFoundCharactersState] = useState({
-//     easy: '',
-//     medium: '',
-//     hard: '',
-//     veryHard: '',
-//   })
-
-//   useEffect(() => {
-//     if (remainingCharactersStatus) {
-//       const foundCharactersMap = foundCharacters.reduce((acc, character) => {
-//         if (game?.names[character]) {
-//           acc[character] = FindingsInfoCSS.foundCharacters
-//         }
-//         return acc
-//       }, {} as Record<string, string>)
-//       setFoundCharactersState(foundCharactersMap)
-//     }
-//   }, [game?.names, foundCharacters, remainingCharactersStatus])
-
-//   return (
-//     <div className={FindingsInfoCSS.findings}>
-//       <ul className={FindingsInfoCSS.ul}>
-//         {Object.entries(game?.names).map(([key, name]) => (
-//           <li
-//             key={key}
-//             id={name}
-//             className={`${FindingsInfoCSS.li} ${foundCharactersState[key]}`}
-//           >
-//             <img
-//               className={FindingsInfoCSS.img}
-//               src={game?.images[key]}
-//               alt={name}
-//             />
-//             {name}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavbarCSS from './Navbar.module.css'
 import FindingsInfoCSS from './FindingsInfo.module.css'
 import FindingsInfo from './FindingsInfo'
@@ -20,96 +20,84 @@ export interface GameProps {
 }
 
 export default function Navbar({ game, foundCharacters }: GameProps) {
-  const location = useLocation()
-  const title = location.pathname === '/'
-  const hideTimer = location.pathname === '/'
-  const hideNumber = location.pathname === '/'
+  const { pathname } = useLocation()
+  const isHomePage = pathname === '/'
+
+  const { names } = game || {}
+
+  const [foundEasy, setFoundEasy] = useState('')
+  const [foundMedium, setFoundMedium] = useState('')
+  const [foundHard, setFoundHard] = useState('')
+  const [foundVeryHard, setFoundVeryHard] = useState('')
 
   const [remainingCharactersStatus, setRemainingCharactersStatus] =
     useState(false)
+  const [renderRemainingFindings, setRenderRemainingFindings] =
+    useState<JSX.Element | null>(null)
   const [remainingCharacterCounter, setRemainingCharacterCounter] =
     useState<number>(4)
-  const [foundCharactersState, setFoundCharactersState] = useState<{
-    foundEasy: string
-    foundMedium: string
-    foundHard: string
-    foundVeryHard: string
-  }>({
-    foundEasy: '',
-    foundMedium: '',
-    foundHard: '',
-    foundVeryHard: '',
-  })
-
-  const renderRemainingFindings = useMemo(() => {
-    return (
-      <FindingsInfo
-        game={game}
-        foundEasy={foundCharactersState.foundEasy}
-        foundMedium={foundCharactersState.foundMedium}
-        foundHard={foundCharactersState.foundHard}
-        foundVeryHard={foundCharactersState.foundVeryHard}
-      />
-    )
-  }, [game, foundCharactersState])
 
   function showFindingsInfo() {
     setRemainingCharactersStatus((prevStatus) => !prevStatus)
   }
 
-  // Update counter
-  useEffect(() => {
-    setRemainingCharacterCounter(4 - foundCharacters.length)
-  }, [foundCharacters])
-
   // Update undiscovered characters menu
   useEffect(() => {
-    const easy = document.querySelector(`#${game?.names.easy}`)
-    const medium = document.querySelector(`#${game?.names.medium}`)
-    const hard = document.querySelector(`#${game?.names.hard}`)
-    const veryHard = document.querySelector(`#${game?.names.veryHard}`)
-
-    let updatedState = { ...foundCharactersState }
-
     foundCharacters.forEach((character) => {
-      if (character === easy?.id) {
-        updatedState.foundEasy = FindingsInfoCSS.foundCharacters
-      } else if (character === medium?.id) {
-        updatedState.foundMedium = FindingsInfoCSS.foundCharacters
-      } else if (character === hard?.id) {
-        updatedState.foundHard = FindingsInfoCSS.foundCharacters
-      } else if (character === veryHard?.id) {
-        updatedState.foundVeryHard = FindingsInfoCSS.foundCharacters
+      if (character === names?.easy) {
+        setFoundEasy(FindingsInfoCSS.foundCharacterStyle)
+      } else if (character === names?.medium) {
+        setFoundMedium(FindingsInfoCSS.foundCharacterStyle)
+      } else if (character === names?.hard) {
+        setFoundHard(FindingsInfoCSS.foundCharacterStyle)
+      } else if (character === names?.veryHard) {
+        setFoundVeryHard(FindingsInfoCSS.foundCharacterStyle)
       }
     })
+    setRemainingCharacterCounter(4 - foundCharacters.length)
+  }, [foundCharacters, names])
 
-    setFoundCharactersState(updatedState)
-  }, [foundCharacters, game, remainingCharactersStatus])
+  // Render undiscovered characters menu
+  useEffect(() => {
+    setRenderRemainingFindings(
+      remainingCharactersStatus ? (
+        <FindingsInfo
+          game={game}
+          foundEasy={foundEasy}
+          foundMedium={foundMedium}
+          foundHard={foundHard}
+          foundVeryHard={foundVeryHard}
+        />
+      ) : null
+    )
+  }, [
+    remainingCharactersStatus,
+    game,
+    foundEasy,
+    foundHard,
+    foundMedium,
+    foundVeryHard,
+  ])
 
   return (
     <>
-      <nav className={NavbarCSS.navbar} id='aa'>
-        {title ? (
-          <div className={NavbarCSS.newGame} id='aa'>
-            Find Us
-          </div>
+      <nav className={NavbarCSS.navbar}>
+        {isHomePage ? (
+          <div className={NavbarCSS.newGame}>Find Us</div>
         ) : (
-          <div className={NavbarCSS.newGame} id='aa'>
+          <div className={NavbarCSS.newGame}>
             <Link to='/' className={NavbarCSS.link}>
               New Game
             </Link>
           </div>
         )}
-        {hideTimer ? null : <div id='timer'>00:00</div>}
-        {hideNumber ? null : (
+        {!isHomePage && <div id='timer'>00:00</div>}
+        {!isHomePage && (
           <div className={NavbarCSS.findingsContainer}>
-            <div
-              className={NavbarCSS.findingsLeft}
-              onClick={() => showFindingsInfo()}
-            >
+            <div className={NavbarCSS.findingsLeft} onClick={showFindingsInfo}>
               <div>{remainingCharacterCounter}</div>
             </div>
-            {remainingCharactersStatus && renderRemainingFindings}
+            {renderRemainingFindings}
           </div>
         )}
       </nav>
